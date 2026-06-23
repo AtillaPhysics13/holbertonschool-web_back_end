1356f70f1cb38c0a372899104043f7d02a77f0e8
AtillaPhysics13 <muradliatilla@gmail.com>
2026-06-23 22:51:31 +0300

    slash

---
+++ NoSQL/dump/logs/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Provides stats about Nginx logs stored in MongoDB."""
+from pymongo import MongoClient
+
+
+def main():
+    """Display Nginx log stats."""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    collection = client.logs.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+    print("Methods:")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_checks = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_checks} status check")
+
+
+if __name__ == "__main__":
+    main()
